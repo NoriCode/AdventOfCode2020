095986342825e0804dc9f6fbcbbd2d337ee3699d
NoriCode <norikitamo@web.de>
2020-12-01 13:46:45 +0100

    Day One

---
+++ cmake-build-debug/CMakeFiles/AdventOfCode2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AdventOfCode2020"
+  "AdventOfCode2020.pdb"
+  "CMakeFiles/AdventOfCode2020.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdventOfCode2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
