601c209166dc66724b3b37f5c26e49680d5b1690
Philip <norikitamo@web.de>
2020-12-02 15:20:03 +0100

    cleaned Code

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         String path = "F:\\Programmierkram\\AdventOfCode2020\\src\\";
 
         System.out.println("\nDay 1");
-        System.out.println(d1T1(path));
-        System.out.println(d1T2(path));
+        d1(path);
 
         System.out.println("\nDay 2");
-        System.out.println(d2T1(path));
-        System.out.println(d2T2(path));
+        d2(path);
+
+        System.out.println("\nDay 3");
+        d3(path);
     }
 
     public static List<String> inputreader(String filename) throws IOException {
@@ -27,106 +27,105 @@ public class Main {
         return input;
     }
 
-    public static int d1T1(String path) throws IOException {
-        List<String> input = inputreader(path + "d1.txt");
-        List<Integer> inputInt = new ArrayList<Integer>();
-        for (String data : input) {
-            inputInt.add(Integer.parseInt(data));
-        }
-        for (int data : inputInt) {
-            for (int data2 : inputInt) {
-                if (data + data2 == 2020) {
-                    return data * data2;
-                }
-            }
-        }
-        return 0;
+
+    public static void kopiervorlage(String path) throws IOException {
+        int correctanswerT1 = 0;
+        int correctanswerT2 = 0;
+        List<String> input = inputreader(path + "d.txt");
+
+
+
+
+        System.out.println("Task One: " + correctanswerT1);
+        System.out.println("Task Two: " + correctanswerT2);
     }
 
-    public static int d1T2(String path) throws IOException {
+
+    public static void d1(String path) throws IOException {
+        int correctanswerT1 = 0;
+        int correctanswerT2 = 0;
+
         List<String> input = inputreader(path + "d1.txt");
-        List<Integer> inputInt = new ArrayList<Integer>();
+        List<Integer> inputInt = new ArrayList<>();
+
         for (String data : input) {
             inputInt.add(Integer.parseInt(data));
         }
         for (int data : inputInt) {
             for (int data2 : inputInt) {
-                for (int data3 : inputInt) {
-                    if (data + data2 + data3 == 2020) {
-                        return data * data2 * data3;
+                if (data + data2 != 2020) {
+                    for (int data3 : inputInt) {
+                        if (data + data2 + data3 == 2020) {
+                            correctanswerT2 = data * data2 * data3;
+                        }
                     }
+                } else {
+                    correctanswerT1 = data * data2;
                 }
             }
         }
-        return 0;
+
+        System.out.println("Task One: " + correctanswerT1);
+        System.out.println("Task Two: " + correctanswerT2);
     }
 
-    public static int d2T1(String path) throws IOException {
-        List<String> input = inputreader(path +"d2.txt");
-        List<String[]> inputdata = new ArrayList<String[]>();
+    public static void d2(String path) throws IOException {
+        int correctanswerT1 = 0;
+        int correctanswerT2 = 0;
 
-        int correctanswer = 0;
-
-        for (String data : input) {
-            inputdata.add(data.split("\\s+"));
-        }
-
-        for (String[] data : inputdata) {
-            String[] spliStr = data[0].split("-");
-            int countLow = Integer.parseInt(spliStr[0]);
-            int countHigh = Integer.parseInt(spliStr[1]);
-            int counter = 0;
-            spliStr = data[1].split(":");
-            String searchChar = spliStr[0];
-
-            char[] password = data[2].toCharArray();
-            for (int j = 0; j < password.length; j++) {
-                if (Character.toString(password[j]).equals(searchChar)) {
-                    counter++;
-                }
-            }
-            if (counter >= countLow && counter <= countHigh) {
-                correctanswer++;
-            }
-        }
-        return correctanswer;
-    }
-
-    public static int d2T2(String path) throws IOException {
-        List<String> input = inputreader(path +"d2.txt");
-        List<String[]> inputdata = new ArrayList<String[]>();
-
-        int correctanswer = 0;
+        List<String> input = inputreader(path + "d2.txt");
+        List<String[]> inputdata = new ArrayList<>();
 
         for (String data : input) {
-            inputdata.add(data.split("\\s+"));
+            inputdata.add(data.split(" "));
         }
 
         for (String[] data : inputdata) {
             boolean found = false;
+            int counter = 0;
+
             String[] spliStr = data[0].split("-");
-            int lowIndex = Integer.parseInt(spliStr[0]);
-            int highIndex = Integer.parseInt(spliStr[1]);
+            int low = Integer.parseInt(spliStr[0]);
+            int high = Integer.parseInt(spliStr[1]);
+
             spliStr = data[1].split(":");
             String searchChar = spliStr[0];
 
             char[] password = data[2].toCharArray();
 
-            if (Character.toString(password[lowIndex - 1]).equals(searchChar)) {
+            for (char c : password) {
+                if (Character.toString(c).equals(searchChar)) {
+                    counter++;
+                }
+            }
+            if (counter >= low && counter <= high) {
+                correctanswerT1++;
+            }
+
+            if (Character.toString(password[low - 1]).equals(searchChar)) {
                 found = true;
             }
-            if (Character.toString(password[highIndex - 1]).equals(searchChar)) {
-                if (!found) {
-                    found = true;
-                } else {
-                    found = false;
-                }
+            if (Character.toString(password[high - 1]).equals(searchChar)) {
+                found = !found;
             }
 
             if (found) {
-                correctanswer++;
+                correctanswerT2++;
             }
         }
-        return correctanswer;
+        System.out.println("Task One: " + correctanswerT1);
+        System.out.println("Task Two: " + correctanswerT2);
+    }
+
+    public static void d3(String path) throws IOException {
+        int correctanswerT1 = 0;
+        int correctanswerT2 = 0;
+        List<String> input = inputreader(path + "d3.txt");
+
+
+
+
+        System.out.println("Task One: " + correctanswerT1);
+        System.out.println("Task Two: " + correctanswerT2);
     }
 }
