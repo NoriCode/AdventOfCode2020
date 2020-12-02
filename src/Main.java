7ccf6768bf6c83b89626661f91551c7d18dc4942
Philip <norikitamo@web.de>
2020-12-02 21:41:30 +0100

    timer for each day

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         String path = "F:\\Programmierkram\\AdventOfCode2020\\src\\inputFiles\\";
 
@@ -94,39 +93,61 @@ public class Main {
     }
 
     public static void d1(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
 
         List<String> input = inputreader(path + "d1.txt");
         List<Integer> inputInt = new ArrayList<>();
 
+        timer1 = System.currentTimeMillis();
         for (String data : input) {
             inputInt.add(Integer.parseInt(data));
         }
+
         for (int data : inputInt) {
             for (int data2 : inputInt) {
-                if (data + data2 != 2020) {
+                if (data + data2 == 2020) {
+                    correctanswerT1 = data * data2;
+                    break;
+                } else {
                     for (int data3 : inputInt) {
                         if (data + data2 + data3 == 2020) {
                             correctanswerT2 = data * data2 * data3;
+                            break;
                         }
                     }
-                } else {
-                    correctanswerT1 = data * data2;
                 }
             }
         }
 
+        timer2 = System.currentTimeMillis();
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d2(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
 
+        int low;
+        int high;
+        int counter;
+
+        boolean found;
+
+        String searchChar;
+        String[] spliStr;
+
         List<String> input = inputreader(path + "d2.txt");
         List<String[]> inputdata = new ArrayList<>();
+
+        timer1 = System.currentTimeMillis();
 
         for (String data : input) {
             /*
@@ -139,20 +160,19 @@ public class Main {
         }
 
         for (String[] data : inputdata) {
-            boolean found = false;
-            int counter = 0;
+            found = false;
+            counter = 0;
 
-            String[] spliStr = data[0].split("-");
-            int low = Integer.parseInt(spliStr[0]);
-            int high = Integer.parseInt(spliStr[1]);
+            spliStr = data[0].split("-");
+            low = Integer.parseInt(spliStr[0]);
+            high = Integer.parseInt(spliStr[1]);
 
-            spliStr = data[1].split(":");
-            String searchChar = spliStr[0];
+            searchChar = data[1];
 
             for (int i = 0; i < data[2].length(); i++) {
                 if (data[2].charAt(i) == searchChar.charAt(0)) {
                     counter++;
-                    if(i == low-1 || i == high-1){
+                    if (i == low - 1 || i == high - 1) {
                         found = !found;
                     }
                 }
@@ -166,237 +186,441 @@ public class Main {
                 correctanswerT2++;
             }
         }
+
+        timer2 = System.currentTimeMillis();
+
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d3(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
         List<String> input = inputreader(path + "d3.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d4(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d4.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d5(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d5.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d6(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d6.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d7(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d7.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d8(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d8.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d9(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d9.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d10(String path) throws IOException {
+        long timer1;
+        long timer2;
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d10.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d11(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d11.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d12(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d12.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d13(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d13.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d14(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d14.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d15(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d15.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d16(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d16.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d17(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d17.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d18(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d18.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d19(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d19.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d20(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d20.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d21(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d21.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d22(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d22.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d23(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d23.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d24(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d24.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 
     public static void d25(String path) throws IOException {
+        long timer1;
+        long timer2;
+
         int correctanswerT1 = 0;
         int correctanswerT2 = 0;
-        List<String> input = inputreader(path + "d3.txt");
+        List<String> input = inputreader(path + "d25.txt");
 
+        timer1 = System.currentTimeMillis();
+        for (String data : input) {
+            System.out.println(data);
+        }
+        timer2 = System.currentTimeMillis();
 
         System.out.println("Task One: " + correctanswerT1);
         System.out.println("Task Two: " + correctanswerT2);
+        System.out.println("Time needed in ms " + (timer2 - timer1));
     }
 }
